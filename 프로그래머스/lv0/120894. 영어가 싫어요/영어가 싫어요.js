329d2de2d3b1678c095b4d14a5215192f5242fd8
Seung gu Shin <77656241+ninefloor@users.noreply.github.com>
2022-10-28 20:04:04 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,9 +1,9 @@
 function solution(numbers) {
   let enNum = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-  while(Number.isNaN(Number(numbers))){
+  
     for (let i = 0; i < enNum.length; i++) {
-      numbers = numbers.replace(enNum[i], i)
+      numbers = numbers.replaceAll(enNum[i], i)
     }
-  }
+  
   return Number(numbers);
 }
