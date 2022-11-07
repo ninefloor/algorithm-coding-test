543d12975aa314a326c29f5fb107bc949a2f303f
Seung gu Shin <77656241+ninefloor@users.noreply.github.com>
2022-11-07 18:36:43 +0900

    [level 0] Title: 모스부호 (1), Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,36 @@
+function solution(letter) {
+  let morse = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+  };
+  let answer = '';
+  let splitStr = letter.split(' ');
+  for (const item of splitStr) {
+    answer += morse[item]
+  }
+  return answer;
+}
